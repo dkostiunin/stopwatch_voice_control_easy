274fc57e4a45557c70982ee31b6706d5a430e9fa
dkostiunin <melochevka@melochevka.ru>
2021-09-02 22:48:37 +0300

    pwa fix err

--- sw.js
+++ sw.js
@@ -1,16 +1,19 @@
-const stat_cache='app_v1',stat_files=['voice_recognition.js','voice_recognition.html','style.css','privacy.html','terms.html','voice_timer.js','stopwatch.js','ru.png','en.png','signal.wav','favicon.svg','manifest.json','maskable_icon.png']
+const stat_cache='app_v1'
+const stat_files=['voice_recognition.js','index.html','style.css','privacy.html','terms.html','voice_timer.js','stopwatch.js','ru.png','en.png','signal.wav','favicon.svg','manifest.json','maskable_icon.png']
+
+//const stat_files=['/voice_recognition.html']
 
 self.addEventListener('install',ev=>{
 	console.log('inst')
-   // ev.waitUntil(caches.open(stat_cache).then(cache=>cache.addAll(stat_files)))
+    ev.waitUntil(caches.open(stat_cache).then(cache=>cache.addAll(stat_files)))
 })
 
  self.addEventListener('activate',async ev=>{console.log('act')
-    //const cache_names = await caches.keys()	
-	//await Promise.all(cache_names.filter(n=>n!==stat_cache).map(n=>caches.delete(n)))	
+    const cache_names = await caches.keys()	
+	await Promise.all(cache_names.filter(n=>n!==stat_cache).map(n=>caches.delete(n)))	
 })
 
-//self.addEventListener('fetch',ev=>{ev.respondWith(cachefirst(ev.request))})
+self.addEventListener('fetch',ev=>{ev.respondWith(cachefirst(ev.request))})
 
 async function cachefirst(req){
 	const c= await caches.match(req)
